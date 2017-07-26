a60728ed933020f30e087aed4777dd3d92380f41
Kesavan PS <pskesavan@tifrh.res.in>
2017-07-26 10:20:04 +0530

    version bumped to 0.2.1; dependency of python-bioformats removed

--- microscoper/__init__.py
+++ microscoper/__init__.py
@@ -1,6 +1,6 @@
 from .io import run
 
 __all__ = ["run"]
-__version__ = "0.2.0"
+__version__ = "0.2.1"
 __author__ = "Kesavan Subburam"
 __email__ = "pskesavan@tifrh.res.in"
--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='microscoper',
-      version='0.2',
+      version='0.2.1',
       description=' A simple wrapper for python-bioformats to convert .vsi CellSense format to TIFF.',
       url='https://www.github.com/pskeshu/microscoper',
       author='Kesavan Subburam',
@@ -9,6 +9,5 @@ setup(name='microscoper',
       packages=['microscoper'],
       install_requires=['numpy>=1.13.1',
                         'tifffile>=0.12.1',
-                        'python-bioformats==1.1.0',
                         'tqdm>=4.11.2'],
       zip_safe=False)
